99f2939da7e6028eb2c49479f16e9de0e2c9d6aa
Ezgi Okka <ezgiokka@gmail.com>
2023-01-12 14:33:04 +0300

    Do css animation exercises

--- animation/02-pop-up/script.js
+++ animation/02-pop-up/script.js
@@ -1,10 +1,10 @@
 const openButton = document.getElementById('trigger-modal');
 const closeButton = document.getElementById('close-modal');
-const backdrop = document.getElementById('backdrop')
+const backdrop = document.getElementById('backdrop');
 
 function toggleModal() {
   const modalDiv = document.querySelector('.popup-modal');
-  const backdrop = document.querySelector('.backdrop')
+  const backdrop = document.querySelector('.backdrop');
   modalDiv.classList.toggle('show');
   backdrop.classList.toggle('show');
 }
